94d6cac9688ce346413459064e56f282abb8908c
Ashwin Gokhale <ashwin.gokhale98@gmail.com>
2018-05-07 22:49:21 -0700

    Added global configs

---
+++ backend/src/config/index.ts
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+declare var CONFIG: any;
+
+CONFIG = {
+	PORT: process.env.PORT || 5000,
+	DB: process.env.DB || 'mongodb://localhost:27017/PH',
+	SECRET: process.env.SECRET || 'my-secret',
+	NODE_ENV: process.env.NODE_ENV || 'development'
+};
