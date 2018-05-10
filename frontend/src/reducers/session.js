2451f717c535829afcabc8eefdf52ad9218a33d9
Ashwin Gokhale <ashwin.gokhale98@gmail.com>
2018-05-09 18:56:29 -0700

    Persist token and user to local storage

--- frontend/src/reducers/session.js
+++ frontend/src/reducers/session.js
@@ -2,22 +2,34 @@ import { AUTH_USER_SET, AUTH_TOKEN_SET } from '../actions';
 
 export default (
 	state = {
-		token: null,
-		user: null
+		token: localStorage.getItem('token') || null,
+		user: JSON.parse(localStorage.getItem('user')) || null
 	},
 	action
 ) => {
 	switch (action.type) {
 		case AUTH_USER_SET: {
+			if (action.user) {
+				localStorage.setItem('user', JSON.stringify(action.user));
+				return {
+					...state,
+					user: {
+						...state.user,
+						...action.user
+					}
+				};
+			}
+
+			localStorage.removeItem('user');
 			return {
 				...state,
-				user: {
-					...state.user,
-					...action.user
-				}
+				user: null
 			};
 		}
 		case AUTH_TOKEN_SET: {
+			action.token
+				? localStorage.setItem('token', action.token)
+				: localStorage.removeItem('token');
 			return {
 				...state,
 				token: action.token
