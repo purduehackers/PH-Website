fc0b1a361659af51a0a78c76aa781ce8b62fca76
Ashwin Gokhale <ashwin.gokhale98@gmail.com>
2018-05-09 19:02:32 -0700

    Added route urls

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -2,5 +2,18 @@ export default {
 	HOME: '/',
 	LOGIN: '/login',
 	SIGNUP: '/signup',
-	PROFILE: '/profile'
+	PROFILE: '/profile',
+	EVENTS: '/events',
+	EVENT: '/event:id',
+	MEMBERS: '/members',
+	MEMBER: '/member:id',
+	CALENDER: '/calender',
+	REPORTS: '/reports',
+	PERMISSIONS: '/permissions',
+	CREDENTIALS: '/credentials',
+	LOCATIONS: '/locations',
+	HACKATHONS: '/hackathons',
+	PROJECTS: '/projects',
+	DEV: '/dev',
+	ANVIL_WIFI: '/anvil-wifi'
 };
